b70457f48872abae0154a4ee3410e49201f020b7
maishaSupritee <maisha.sup@gmail.com>
2025-08-15 15:51:48 -0700

    fix: add authentication protection to user update and delete routes
    Signed-off-by: maishaSupritee <maisha.sup@gmail.com>

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,13 +7,16 @@ import {
   deleteUser,
 } from "../controllers/userController.js";
 import { validateUserInput } from "../middlewares/inputValidator.js";
+import protect from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/users", validateUserInput, createUser);
 router.get("/users", getAllUsers);
 router.get("/users/:id", getUserById);
-router.put("/users/:id", validateUserInput, updateUser);
-router.delete("/user/:id", deleteUser);
+
+// Protected routes
+router.put("/users/:id", protect, validateUserInput, updateUser);
+router.delete("/users/:id", protect, deleteUser);
 
 export default router;
